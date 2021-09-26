7904920af15381d068e7b00c01d18c9fdfc3e6f1
jardimdanificado <74869343+jardimdanificado@users.noreply.github.com>
2021-09-26 10:47:04 -0300

    fixed (this is only for debian)

--- build_nosudo.sh
+++ build_nosudo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt install libncurses5-dev libxdo build-essential libx11 libxtst libc-bin linux-libc-dev
+apt install libncurses5-dev libxdo-dev libxdo3 build-essential libx11-dev libxtst-dev libc-bin linux-libc-dev
 
 gcc main.c -o air.run -lncurses -lxdo
